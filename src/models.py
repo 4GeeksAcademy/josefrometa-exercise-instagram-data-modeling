aa19c26d12dc12b63046bdb6dd35a41d7b19ae53
josefrometa <73325073+josefrometa@users.noreply.github.com>
2023-10-30 08:51:46 +0000

    primer commit

--- src/models.py
+++ src/models.py
@@ -1,29 +1,71 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, LargeBinary, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
+# Definir las tablas user, post, media, comment y follower/ Listo
+# Llenar el contenido de las tablas segun lo solicitado/ Listo
+# Establecer las relaciones entre tablas de manera correcta:
+# 1ra Relacion User-Follower relacion one to many de ambas partes (Porque un usurio puede tener muchos seguidores y los seguidores son muchos usurios) / Listo
+# 2da Reclacion User-Comment relacion one to many (Un usurio puede tener muchos comentarios pero un comentario no puede tener muchos usurios) /Listo
+# 3ra Relacion User-Post relacion one to many (Un usurio puede hacer muchas publicaciones pero esas publicaciones pertenecen a un solo usurio) /Listo
+# 4ta Relacion Post-Comment relacion one to many (Una publicacion puede tener muchos cometarios pero un comentario pertenece a una publicacion) /Listo
+# 5ta Relacion Post-Media relacion one to one /Listo
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    username = Column(String(30), nullabble=False, unique=True)
+    password = Column(String(250), nullabble=False)
+    email = Column(String(100), nullabble=False, unique=True)
+    birth_date = Column(DateTime(), nullabble=False)
+    followers = relationship("Followers", uselist=True, backref='user')
+    followers_id = Column(Integer(), ForeignKey='followers.id')
+    commets_id = Column(Integer(), ForeignKey='comment.id')
+    post_id =   Column(Integer(), ForeignKey='post.id')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Followers(Base):
+    __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullabble=False)
+    username = Column(String(30), nullabble=False, unique=True)
+    followers_of_follower = Column(Integer())
+    users = relationship("User", uselist=True, backref='followers')
+    users_id = Column(Integer(), ForeignKey='user.id')
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_post = relationship("User", uselist=True, backref='post')
+    comment_id = Column(Integer(), ForeignKey='commet')
+    media = relationship("Media", uselist=False, backpopulates="post")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    user_comments = relationship("User", uselist=True, backref='comment')
+    post_comments  = relationship("Post", uselist=True, backref='comment')
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(LargeBinary)
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey='post.id')
+    post = relationship("Post", backpopulates="media")
+
+
 
     def to_dict(self):
         return {}
